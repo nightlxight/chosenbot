f3e97338e1c4386e30221cd8c2c48ea368e5d346
nightlxight <itsbeadj@gmail.com>
2021-10-06 18:18:14 +0300

    🎨 Added colors, util folder and file(s) in it and sendEmbedFunction() function

--- commands/invite.js
+++ commands/invite.js
@@ -2,20 +2,29 @@ var fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const yaml = require('js-yaml');
-
+const { random_color } = require('../util/colors/random.json');
+const { inviteDescription } = require('../resources/invite/desc.yml');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('invite')
-        .setDescription("Sends invite link to bot!"),
+        .setDescription(`${inviteDescription}`),
     async execute(interaction) {
         let embed = new MessageEmbed()
-            .setColor("RANDOM")
+            .setColor(random_color)
             .setTitle('Bot Invite Link')
             .setDescription('Invite link can be found below')
             .addField('Invite Link:', 'your bots invite link here', true) /* TO GET YOUR INVITE LINK, PLEASE GO TO https://discordapi.com/permissions.html */
             .setFooter(`${interaction.user.tag} | invite link`)
             .setTimestamp();
-        return interaction.reply({embeds: [embed] });
+        function sendEmbedFunction() {
+            try {
+                return interaction.reply({ embeds: [embed] });
+            } catch (error) {
+                // * interaction.reply({content: "There was an error!"});
+                console.error(error);
+            };
+        };
+        sendEmbedFunction();
     },
 };
